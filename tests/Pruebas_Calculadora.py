28cc5d9d8060734d8efcddab77b88d3c3faddfe0
IsaacVega <isacvega361@gmail.com>
2022-11-16 16:49:38 -0300

    Supuesta solución

--- tests/Pruebas_Calculadora.py
+++ tests/Pruebas_Calculadora.py
@@ -1,13 +1,13 @@
 import unittest
-from calculadora.app import sumar, dividir, restar, multiplicar
+from Calculadora.app import sumar, dividir, restar, multiplicar
 
-class PruebaCalculadora(unittest.TestCase):
+class Pruebas_Calculadora(unittest.TestCase):
     def prueba_sumar(self):
         result = sumar(2,3)
         self.assertEqual(result, 5, "El resultado debería ser 5")
 
     def prueba_restar(self):
-        result1 = restar(5,4)
+        result = restar(5,4)
         self.assertEqual(result, 1, "El resultado debería ser 1")
 
     def prueba_multiplicar(self):
