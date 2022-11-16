ebec463fb9ab9ff3a25095f9026715ce9ceaa6d5
BuildTools <isaacvega361@gmail.com>
2022-11-16 02:55:50 -0300

    Primer commit, agregar archivos al repositorio

---
+++ Pruebas/Pruebas_Calculadora.py
@@ -0,0 +1,23 @@
+import unittest
+from calculadora.app import sumar, dividir, restar, multiplicar
+
+class PruebaCalculadora(unittest.TestCase):
+    def prueba_sumar(self):
+        result = sumar(2,3)
+        self.assertEqual(result, 5, "El resultado debería ser 5")
+
+    def prueba_restar(self):
+        result1 = restar(5,4)
+        self.assertEqual(result, 1, "El resultado debería ser 1")
+
+    def prueba_multiplicar(self):
+        result = multiplicar(3,4)
+        self.assertEqual(result, 12, "El resultado debería ser 12")
+
+    def prueba_dividir(self):
+        result = dividir(12,3)
+        self.assertEqual(result, 4, "El resultado debería ser 4")
+
+
+if __name__ == "__main__":
+    unittest.main()
